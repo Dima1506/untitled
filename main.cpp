f8565b94c768a639c46a041a82dd780f4ce6effb
Dmitry <ichyotckin@yandex.ru>
2017-08-19 21:05:40 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,37 @@
+// Реализуйте функции и методы классов и при необходимости добавьте свои
+
+class Date {
+public:
+    Date(int year,int month,int day){
+
+    }
+    int GetYear() const;
+    int GetMonth() const;
+    int GetDay() const;
+private:
+    int year,month,day;
+};
+
+bool operator<(const Date& lhs, const Date& rhs);
+
+class Database {
+public:
+    void AddEvent(const Date& date, const string& event);
+    bool DeleteEvent(const Date& date, const string& event);
+    int  DeleteDate(const Date& date);
+
+    /* ??? */ Find(const Date& date) const;
+
+    void Print() const;
+};
+
+int main() {
+    Database db;
+
+    string command;
+    while (getline(cin, command)) {
+        // Считайте команды с потока ввода и обработайте каждую
+    }
+
+    return 0;
+}
