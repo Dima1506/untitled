d3b8f0e599921f67eca8d48e4d3c4d1fb0d2ac87
Dmitry <ichyotckin@yandex.ru>
2017-08-19 21:19:47 +0300

    complete class Date

--- main.cpp
+++ main.cpp
@@ -1,19 +1,43 @@
 // Реализуйте функции и методы классов и при необходимости добавьте свои
-
+#include <string>
+#include <map>
+#include <iostream>
+using namespace std;
 class Date {
 public:
     Date(int year,int month,int day){
-
+        this->year=year;
+        this->month=month;
+        this->day=day;
     }
-    int GetYear() const;
-    int GetMonth() const;
-    int GetDay() const;
+    int GetYear() const{
+        return year;
+    }
+    int GetMonth() const{
+        return month;
+    }
+    int GetDay() const{
+        return day;
+    }
 private:
     int year,month,day;
 };
 
-bool operator<(const Date& lhs, const Date& rhs);
-
+bool operator<(const Date& lhs, const Date& rhs){
+    if(lhs.GetYear()!=rhs.GetYear()){
+        return lhs.GetYear()<rhs.GetYear();
+    } else {
+        if (lhs.GetMonth() != rhs.GetMonth()) {
+            return lhs.GetMonth() < rhs.GetMonth();
+        } else{
+            if(lhs.GetDay()!=rhs.GetDay()){
+                return lhs.GetDay()<rhs.GetDay();
+            } else{
+                return false;
+            }
+        }
+    }
+}
 class Database {
 public:
     void AddEvent(const Date& date, const string& event);
